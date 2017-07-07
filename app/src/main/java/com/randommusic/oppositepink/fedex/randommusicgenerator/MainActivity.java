b5c0d5c629d6a2f0f401ea09429f8e291f52b1b1
bncbodrogi <bncbodrogi@gmail.com>
2017-07-07 12:13:49 +0200

    drawer iz pimp

--- app/src/main/java/com/randommusic/oppositepink/fedex/randommusicgenerator/MainActivity.java
+++ app/src/main/java/com/randommusic/oppositepink/fedex/randommusicgenerator/MainActivity.java
@@ -316,17 +316,15 @@ public class MainActivity extends AppCompatActivity implements MediaPlayerContro
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_sleep) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_timer) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_shuffle) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_mood) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_exit) {
 
         }
 
